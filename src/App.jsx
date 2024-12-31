0471a285557f86ef06d28a8e96768dd8e10b1163
pukhraj1002 <119615554+Pawanraj2004@users.noreply.github.com>
2024-12-31 15:51:45 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./home/Home";
 import Detection from "./TeachableMachine/Detection";
-import DiagnosisForm from "./diagnose/DiagnosisForm";
 
 const App = () => {
   return (
@@ -11,7 +10,6 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/detect" element={<Detection />} />
-        <Route path="/diagnose" element={<DiagnosisForm />} />
       </Routes>
     </div>
   );
